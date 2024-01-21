1aa574be3ea3f41af0c94ba15c33bb4b0fc5981e
ramya-ramanathan <ramyaramanathan1996@gmail.com>
2024-01-21 09:10:36 +0000

    implement calculator.py

--- challenge/calculator.py
+++ challenge/calculator.py
@@ -1,2 +1,18 @@
 class Calculator:
-    pass
+    def __init__(self,error_t1, error_t2, error_t3, error_t4) -> None:
+        self._types = []
+        self._types.append(error_t1)
+        self._types.append(error_t2)
+        self._types.append(error_t3)
+        self._types.append(error_t4)
+        self.error = None
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type in self._types:
+            self.error = exc_type
+            return True
+        return False
+    
